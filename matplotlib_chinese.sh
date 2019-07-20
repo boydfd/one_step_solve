9924de0b8056c9becc664e80f233cbc6a7a87ccd
rlin <rlin@thoughtworks.com>
2019-07-20 13:54:28 +0800

    [@boydfd] refactor: download ttf from github.

--- matplotlib_chinese.sh
+++ matplotlib_chinese.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 site_packages=$(python3 -c "import site; print(site.getsitepackages()[0])")
-cp ./resources/SimHei.ttf ${site_packages}/matplotlib/mpl-data/fonts/ttf/
-rm -rf ~/.matplotlib/*.cache
-rm ~/.matplotlib/fontlist-v310.json
+curl https://github.com/boydfd/one_step_solve/raw/master/resources/SimHei.ttf -o ${site_packages}/matplotlib/mpl-data/fonts/ttf/SimHei.ttf
 backup=${site_packages}/matplotlib/mpl-data/matplotlibrc_backup
 rc=${site_packages}/matplotlib/mpl-data/matplotlibrc
 
@@ -31,3 +29,6 @@ path=${rc}
 add_config "${font_family}" "${path}"
 add_config "${sans_serif}" "${path}"
 add_config "${axes_unicode_minus}" "${path}"
+
+rm -rf ~/.matplotlib/*.cache
+rm ~/.matplotlib/fontlist-v310.json
